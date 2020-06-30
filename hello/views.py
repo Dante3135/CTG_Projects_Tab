1d0a674fd8338966744eac611ceaa804c4bf82b8
Dante Angelone <dante31@Dantes-MBP.home>
2020-06-30 11:13:41 -0400

    first

---
+++ hello/views.py
@@ -0,0 +1,63 @@
+import re
+from datetime import datetime
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+from django.views.generic import ListView
+
+
+# Add this code elsewhere in the file:
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
+
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def project(request):
+    return render(request, "hello/project.html")
+
+def mapping(request):
+    return render(request, 'hello/mapping.html')
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
+
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+
+    content = 'hello/hello_there.html'
+    return HttpResponse(content)
